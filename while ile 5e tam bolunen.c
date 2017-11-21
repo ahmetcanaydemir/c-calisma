45cf78d42c9faee9c0fa9bc27c75540a482fd633
Ahmet Can Aydemir <ahmetcanaydemir@users.noreply.github.com>
2017-11-21 21:44:25 +0300

    Create while ile 5e tam bolunen.c

---
+++ while ile 5e tam bolunen.c
@@ -0,0 +1,27 @@
+/*
+200 den baslayarak 0'a kadar 5 e tam  bölünen sayıları while döngüsü ile listeleyen program
+*/
+
+#include <stdio.h>
+int main()
+{
+	int i=200;
+	while(i>=0)
+	{
+		if(i%5==0)
+		{
+			printf("%d ",i);
+		}
+		i--;
+	}
+  
+  return 0;
+}
+
+
+
+
+
+
+
+
