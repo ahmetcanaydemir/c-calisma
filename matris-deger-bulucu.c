44ab2ee903a05e15a689282208907389ee5a2e66
Ahmet Can Aydemir <ahmetcanaydemir@users.noreply.github.com>
2018-01-07 21:10:11 +0300

    Update matris-deger-bulucu.c

--- matris-deger-bulucu.c
+++ matris-deger-bulucu.c
@@ -19,14 +19,14 @@ char islem(char dizi[5][5], char karakter1, char karakter2)
 int main()
 {
 	char dizi[5][5] = { { ' ','a','b','c','d'},
-						{ 'a','d','c','b','a' },
-						{ 'b','c','b','a','d' },
-						{ 'c','b','a','c','d' },
-						{ 'd','a','c','d','b' } };
+			  { 'a','d','c','b','a' },
+			  { 'b','c','b','a','d' },
+			  { 'c','b','a','c','d' },
+			  { 'd','a','c','d','b' } };
 	char karakter1, karakter2;
-    printf("harf gir:");
-    scanf("%c %c",&karakter1,&karakter2);
-    printf("islem sonucu: %c\t",islem(dizi,karakter1,karakter2));
+	printf("harf gir:");
+	scanf("%c %c",&karakter1,&karakter2);
+	printf("islem sonucu: %c\t",islem(dizi,karakter1,karakter2));
 
 	getch();
 	return 0;
